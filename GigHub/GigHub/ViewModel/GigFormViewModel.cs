005c84efe4ef36fea0adc0a41de72c55606f8301
ngohungphuc <ngohungphuc95@gmail.com>
2017-03-29 08:20:18 +0700

    form to add gigs

---
+++ GigHub/GigHub/ViewModel/GigFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using GigHub.Models;
+
+namespace GigHub.ViewModel
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
