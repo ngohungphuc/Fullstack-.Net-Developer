caa86c69bb814240517477ee8cc714e67c93d0ab
ngohungphuc <ngohungphuc95@gmail.com>
2017-03-29 10:59:39 +0700

    Validation

--- GigHub/GigHub/ViewModel/GigFormViewModel.cs
+++ GigHub/GigHub/ViewModel/GigFormViewModel.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using GigHub.Models;
 
 namespace GigHub.ViewModel
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
 
-        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse($"{Date} {Time}");
+        }
     }
 }
